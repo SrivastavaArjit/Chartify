7483b27a01715e36e55ba1c41071795558cda0ba
Arjit <82926673+SrivastavaArjit@users.noreply.github.com>
2025-05-01 22:49:07 +0530

    remove legacy server

--- server/index.js
+++ server/index.js
@@ -3,32 +3,33 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import DashboardData from "./schema.js";
 import dotenv from "dotenv";
+import serverless from "serverless-http";
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
-dotenv.config();
-
+const uri = process.env.MONGODB_URI;
 const clientOptions = {
   serverApi: { version: "1", strict: true, deprecationErrors: true },
 };
 
-const uri = process.env.MONGODB_URI;
+let isConnected = false;
 
-async function run() {
+async function connectDB() {
+  if(isConnected) return;
   try {
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
+    isConnected = true;
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } catch {
-    console.log("Error Occurred");
+  } catch (err) {
+    console.error("MongoDB Connection Error Occurred", err);
   }
 }
-run().catch(console.dir);
 
 //Enabling Cors
 app.use((req, res, next) => {
@@ -46,10 +47,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/dashboard", async (req, res) => {
-  const fetchData = await DashboardData.find({});
-
-  res.send(fetchData);
+  // const fetchData = await DashboardData.find({});
+  try{
+    await connectDB();
+    const data = await DashboardData.find({});
+    res.status(200).json(data);
+  }catch(err){
+    res.status(500).json({error: "Failed to fetch dashboard data"});
+  }
 });
-app.listen(port, () => {
-  console.log("listening on port 3000...");
-});
+// app.listen(port, () => {
+//   console.log("listening on port 3000...");
+// });
+export default serverless(app);
